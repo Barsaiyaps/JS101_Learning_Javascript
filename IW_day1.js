54534df6145192e8a85ec5f960d5a839354c73bc
Barsaiyaps <pankajbars@gmail.com>
2023-09-03 08:06:23 +0530

    file added1

---
+++ IW_day1.js
@@ -0,0 +1,7 @@
+let today = 2023;
+let birth_date = 1995;
+if (today - birth_date >= 13 && today - birth_date <= 19) {
+  console.log("Teenage")
+} else if (today - birth_date >= 20 && today - birth_date <= 29) {
+  console.log("Twenties")
+}
